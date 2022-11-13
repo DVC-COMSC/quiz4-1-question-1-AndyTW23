2acffabb729e514bf4b7dfc21fdb2b5f267edefb
Kyu Lee <kwooler@gmail.com>
2022-11-12 20:16:24 -0800

    See the file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 // Complete this program
 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+// 	int choice;
+// 	double width, height, radius;
+// 	double area;
+
+// 	cout << "Geometry Calculator\n";
+// 	cout << "\t1. Calculate the Area of Circle\n";
+// 	cout << "\t2. Calculate the Area of Rectangle\n";
+// 	cout << "\t3. Calculate the Area of Triangle\n";
+// 	cout << "\t4. Quit\n\n";
+// 	cout << "Enter your choice (1-4): ";
+// 	cin >> choice;
+
+//         //************************
+//         // Your code
+//         //************************
+
+//         cout << area << endl;
+// }
 #include <iostream>
 using namespace std;
 
@@ -17,9 +40,30 @@ int main()
 	cout << "Enter your choice (1-4): ";
 	cin >> choice;
 
-        //************************
-        // Your code 
-        //************************
-        
-        cout << area << endl;
+	switch (choice)
+	{
+	case 1:
+		cout << "Enter the radius: ";
+		cin >> radius;
+		area = 3.14 * radius * radius;
+		break;
+	case 2:
+		cout << "Enter the width and height";
+		cin >> width >> height;
+		area = width * height;
+		break;
+	case 3:
+		cout << "Enter the width and height";
+		cin >> width >> height;
+		area = width * height * 0.5;
+		break;
+	case 4:
+		cout << "Program Stopped";
+		return 0;
+		break;
+	default:
+		cout << "Wrong Number\n";
+		return 0;
+	}
+	cout << area << endl;
 }
